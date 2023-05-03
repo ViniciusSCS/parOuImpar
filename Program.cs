ec0e46506b1e097b1ce980f2637f984ec6afa744
Vinícius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2023-05-03 19:55:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string parOuImpar;
+
+            Console.Write("Entre com um número inteiro: ");
+            x = int.Parse(Console.ReadLine());
+
+            parOuImpar = x % 2 == 0 ? "PAR!" : "IMPAR!";
+
+            Console.WriteLine($"O valor é {parOuImpar}");
+        }
+    }
+}
